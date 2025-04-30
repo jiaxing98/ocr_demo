fc53e7a9e765b6a7054f5e3f6cf46c980007855a
WAN JIA XING <jiaxing.wan@redone.com.my>
2025-04-30 15:11:45 +0800

    WIP

--- lib/presentation/text_recognizer/text_recognizer_page.dart
+++ lib/presentation/text_recognizer/text_recognizer_page.dart
@@ -1,9 +1,12 @@
 import 'dart:io';
 
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+import 'package:image/image.dart' as img;
 import 'package:image_picker/image_picker.dart';
 import 'package:ocr_demo/presentation/text_recognizer/widgets/action_buttons.dart';
+import 'package:pdfx/pdfx.dart';
 
 class TextRecognizerPage extends StatefulWidget {
   const TextRecognizerPage({super.key});
@@ -63,7 +66,7 @@ class _TextRecognizerPageState extends State<TextRecognizerPage> {
               _getImage(ImageSource.gallery);
             },
             onScanPdf: () {},
-            onSelectPdf: () {},
+            onSelectPdf: _selectPdf,
           ),
           if (_image != null)
             Padding(
@@ -75,7 +78,8 @@ class _TextRecognizerPageState extends State<TextRecognizerPage> {
     );
   }
 
-  Future _getImage(ImageSource source) async {
+  //region Image Processing
+  Future<void> _getImage(ImageSource source) async {
     setState(() {
       _image = null;
       _path = null;
@@ -86,7 +90,7 @@ class _TextRecognizerPageState extends State<TextRecognizerPage> {
     }
   }
 
-  Future _processFile(String path) async {
+  Future<void> _processFile(String path) async {
     setState(() {
       _image = File(path);
     });
@@ -99,4 +103,58 @@ class _TextRecognizerPageState extends State<TextRecognizerPage> {
       setState(() {});
     }
   }
+  //endregion
+
+  Future<void> _selectPdf() async {
+    setState(() {
+      _image = null;
+      _path = null;
+    });
+
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['pdf'],
+    );
+    if (result == null) return;
+
+    final document = await PdfDocument.openFile(result.files.single.path!);
+    final textFromImages = <String>[];
+    for (int i = 0; i < document.pagesCount; i++) {
+      final page = await document.getPage(i + 1); // ! must start from 1
+      final renderedPage = await page.render(
+        width: page.width * 2,
+        height: page.height * 2,
+        format: PdfPageImageFormat.png,
+      );
+      if (renderedPage == null) continue;
+
+      // ! renderedPage.bytes is PNG-encoded bytes, NOT raw pixel buffer
+      // ! thus need to decode PNG bytes to raw pixel data
+      final image = img.decodeImage(renderedPage.bytes)!;
+      final rawBytes = image.toUint8List();
+
+      // todo: first - i need to check why imageformat invalid
+      final inputImage = InputImage.fromBytes(
+        bytes: rawBytes,
+        metadata: InputImageMetadata(
+          size: Size(image.width * 2, image.height * 2),
+          rotation: InputImageRotation.rotation0deg,
+          // ! image package uses bgra8888 format (4 bytes per pixel (Blue, Green, Red, Alpha))
+          format: InputImageFormat.bgra8888,
+          // ! since each pixel takes 4 bytes, each row has (image.width) pixel * 4
+          bytesPerRow: image.width * 4,
+        ),
+      );
+
+      // final recognizedText = await _textRecognizer.processImage(inputImage);
+      // textFromImages.add(recognizedText.text);
+    }
+
+    _text = 'Recognized text:\n\n${textFromImages.join('\n')}';
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  Future<void> _scanPdf() async {}
 }
